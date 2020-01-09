2eac506a8223ee7f459a44624b56a6d0c1b8159b
mohammed osama <mohammedosama@ieee.org>
2020-01-09 19:13:21 +0200

    setup a basic http request

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+func handleRequests(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to my badddd site.");
+
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	// runs at all available methods.
+	http.HandleFunc("/", handleRequests);
+	http.ListenAndServe(":3000", nil);
 }
