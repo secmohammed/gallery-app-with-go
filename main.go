d6d0bc5bb5e94e091f4261d827bdabbd771b11c9
mohammed osama <mohammedosama@ieee.org>
2020-01-09 19:38:33 +0200

    added a content type header.

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"net/http"
 )
+
 func handleRequests(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to my badddd site.");
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Welcome to my badddd site.")
 
 }
 
 func main() {
 	// runs at all available methods.
-	http.HandleFunc("/", handleRequests);
-	http.ListenAndServe(":3000", nil);
+	http.HandleFunc("/", handleRequests)
+	http.ListenAndServe(":3000", nil)
 }
