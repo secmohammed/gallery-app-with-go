0aec8e33fa5dfdba29a111e7138bdbad57e2bf76
mohammed osama <mohammedosama@ieee.org>
2020-01-11 01:58:08 +0200

    parse form request data into schema

--- controllers/users.go
+++ controllers/users.go
@@ -4,27 +4,44 @@ import (
     "fmt"
     "net/http"
 
+    "github.com/gorilla/schema"
     "lenslocked.com/resources/views"
 )
 
 // ShowRegisterForm function to show the form
-func ShowRegisterForm() *Users {
-    return &Users{
+func ShowRegisterForm() *View {
+    return &View{
         NewView: views.NewView("layout", "resources/views/auth/register.gohtml"),
     }
 }
 
-//ParseRegisterForm to parse the registration form when submitted.
-func ParseRegisterForm(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.PostFormValue("email"))
+// RegisterFormRequest type.
+type RegisterFormRequest struct {
+    Email    string `schema:"email"`
+    Password string `schema:"password"`
 }
 
-// Users type.
-type Users struct {
+//ParseRegisterForm to parse the registration form when submitted.
+func ParseRegisterForm(w http.ResponseWriter, r *http.Request) {
+    // parseForm must be called in order to fill the postForm with the data coming from the input form data.
+    if err := r.ParseForm(); err != nil {
+        panic(err)
+    }
+    decoder := schema.NewDecoder()
+    var form RegisterFormRequest
+
+    if err := decoder.Decode(&form, r.PostForm); err != nil {
+        panic(err)
+    }
+    fmt.Fprintln(w, form)
+}
+
+// View type.
+type View struct {
     NewView *views.View
 }
 
 // Render Method to render the parsed view.
-func (u *Users) Render(w http.ResponseWriter, r *http.Request) {
+func (u *View) Render(w http.ResponseWriter, r *http.Request) {
     u.NewView.Render(w, nil)
 }
