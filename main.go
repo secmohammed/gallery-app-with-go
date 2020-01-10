a27729412cfc7240911b7ecca6c7f8c32c12bfb5
mohammed osama <mohammedosama@ieee.org>
2020-01-10 15:09:08 +0200

    create basic homepage.

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+var homeTemplate *template.Template
+
+func handleHomeRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1> Hello, we are at homepage </h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 
 }
 func handleContactRequest(response http.ResponseWriter, request *http.Request) {
@@ -25,9 +30,14 @@ func customNotFoundPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Sorry, but we couldn't find the page you are looking for.")
 }
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	router := mux.NewRouter()
 	router.NotFoundHandler = http.HandlerFunc(customNotFoundPage)
-	router.HandleFunc("/", home)
+	router.HandleFunc("/", handleHomeRequest)
 	router.HandleFunc("/faq", handleFAQRequest)
 	router.HandleFunc("/contact", handleContactRequest)
 	http.ListenAndServe(":3000", router)
