6b65b39d375ce5934bac14527e87226b731fa507
mohammed osama <mohammedosama@ieee.org>
2020-01-18 07:31:49 +0200

    passwords for users are hashed

--- models/user.go
+++ models/user.go
@@ -4,8 +4,9 @@ import (
     "errors"
 
     "github.com/jinzhu/gorm"
-
+    "golang.org/x/crypto/bcrypt"
     "lenslocked.com/utils"
+
     // load the database connection.
     _ "lenslocked.com/utils"
 )
@@ -15,8 +16,10 @@ var db *gorm.DB = utils.GetDatabaseConnection()
 
 // ErrorNotFound is returned when a resource cannot be found.
 var ErrorNotFound = errors.New("model: resource not found")
-// ErrorInvalidID will be thrown in case of the id is invalid or equal to zero.
+
+//ErrorInvaildID will be thrown in case of the id is invalid or equal to zero.
 var ErrorInvaildID = errors.New("models: ID provided was invalid")
+
 // User type
 type User struct {
     gorm.Model
@@ -27,12 +30,19 @@ type User struct {
 
 //Create function is used to create a users record
 func Create(user *User) error {
+    hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+    if err != nil {
+        return err
+    }
+    user.Password = string(hashedBytes)
     return db.Create(user).Error
 }
+
 // Update will update the provided user with all of the data passed through the user object.
 func Update(user *User) error {
     return db.Save(user).Error
 }
+
 // ByID will look up the users using the given id.
 func ByID(id uint) (*User, error) {
     var user User
@@ -40,6 +50,7 @@ func ByID(id uint) (*User, error) {
     err := first(query, &user)
     return &user, err
 }
+
 //Delete function will delete the user with the provided id.
 func Delete(id uint) error {
     // gorm will delete all of the records if the id equals to zero.
@@ -54,6 +65,7 @@ func Delete(id uint) error {
     }
     return db.Delete(&user).Error
 }
+
 // ByEail function will lok up the users using the given email.
 func ByEmail(email string) (*User, error) {
     var user User
@@ -61,12 +73,13 @@ func ByEmail(email string) (*User, error) {
     err := first(query, &user)
     return &user, err
 }
+
 // first will quer using the provided database query and it will get the first item returned and place it into dst, if nothing is returned/found in the query, it will return error not found
 func first(db *gorm.DB, dst interface{}) error {
     err := db.First(dst).Error
     switch err {
     case nil:
-        return  nil
+        return nil
     case gorm.ErrRecordNotFound:
         return ErrorNotFound
     default:
--- models/user_test.go
+++ models/user_test.go
@@ -1,31 +1,18 @@
 package models
-import(
+
+import (
     "testing"
-    "fmt"
     // setting up gorm with postgres connection
     _ "github.com/jinzhu/gorm/dialects/postgres"
-
-    "github.com/jinzhu/gorm"
-
 )
-
 
 //env, err = godotenv.Unmarshal("APP_ENV=testing")
 //err = godotenv.Write(env, "../.env")
 func TestCreateUser(t *testing.T) {
-    db, err := testingUserService()
-    if err != nil {
-        t.Fatal(err)
-    }
     user := User{
-        Name: "Mohammd Osama",
+        Name:     "Mohammd Osama",
         Password: "helloworld",
-        Email: "mohammeadosama@ieee.org",
-
-    }
-    err = Create(&user)
-    if err != nil {
-        t.Fatal(err)
+        Email:    "mohammeadosama@ieee.org",
     }
     if user.ID == 0 {
         t.Errorf("Expected ID > 0, Received %d", user.ID)
